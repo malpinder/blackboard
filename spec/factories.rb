bd5cb2849e32eb6ea7fc92c21a0226f4a022c0cc
Sarah Pinder <r.s.pinder@gmail.com>
2013-06-26 15:56:22 +0100

    Add github & image url to the user factories.

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,5 +6,7 @@ FactoryGirl.define do
     nickname "ada"
     provider "github"
     uid "12345"
+    github_url "https://example.github.com/adal"
+    image_url "https://example.gravatar.com/pictures/adal"
   end
 end
