f2f951d95f053622af46ba4cf8732c10ecaab056
Sarah Pinder <r.s.pinder@gmail.com>
2013-07-02 21:48:33 +0100

    [+AS] Add validations to GoalCompletions.
    Don't allow goal_id or user_projects_id to be nil,
    make sure there's only one for each.

--- app/models/goal_completion.rb
+++ app/models/goal_completion.rb
@@ -1,4 +1,7 @@
 class GoalCompletion < ActiveRecord::Base
   belongs_to :goal
   belongs_to :user_project
+
+  validates :user_project_id, presence: true
+  validates :goal_id, presence: true, uniqueness: { scope: :user_project_id }
 end
