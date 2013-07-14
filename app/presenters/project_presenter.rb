2c484e0ca2d30478baa67a6c939c0aedab12f956
Sarah Pinder <r.s.pinder@gmail.com>
2013-07-14 14:01:16 +0100

    [+AS] ProjectPresenter user_project finders
    Returns them grouped by status, does not return
    the user's user projects.
    
    This will all be refactored soon.

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -24,6 +24,19 @@ class ProjectPresenter
     user.user_projects.find_by(project_id: project.id)
   end
 
+  def worked_on_user_projects
+    user_projects.reject(&:complete?)
+  end
 
+  def completed_user_projects
+    user_projects.select(&:complete?)
+  end
+
+  def user_projects
+    return @user_projects if @user_projects
+    @user_projects = @project.user_projects.order(created_at: :desc)
+    @user_projects = @user_projects.where.not(user_id: user.id) if user.present?
+    @user_projects
+  end
 
 end
--- spec/presenters/project_presenter_spec.rb
+++ spec/presenters/project_presenter_spec.rb
@@ -41,4 +41,48 @@ describe ProjectPresenter do
     end
   end
 
+  describe "#worked_on_user_projects" do
+    let(:project) { FactoryGirl.create(:project) }
+    subject { ProjectPresenter.new(project).worked_on_user_projects }
+    it "returns user projects with no progress" do
+      user_project = FactoryGirl.create(:user_project, project: project)
+      expect(subject).to include user_project
+    end
+    it "returns user projects with some progress" do
+      user_project = FactoryGirl.create(:user_project, :in_progress, project: project)
+      expect(subject).to include user_project
+    end
+    it "does not return user projects that are complete" do
+      user_project = FactoryGirl.create(:user_project, :complete, project: project)
+      expect(subject).to_not include user_project
+    end
+    it "does not return user_projects for the user" do
+      user_project = FactoryGirl.create(:user_project, :in_progress, project: project)
+      user = user_project.user
+      expect(ProjectPresenter.new(project, user).worked_on_user_projects).to_not include user_project
+    end
+  end
+
+  describe "#completed_user_projects" do
+    let(:project) { FactoryGirl.create(:project) }
+    subject { ProjectPresenter.new(project).completed_user_projects }
+    it "doesn not return user projects with no progress" do
+      user_project = FactoryGirl.create(:user_project, project: project)
+      expect(subject).to_not include user_project
+    end
+    it "does not return user projects with some progress" do
+      user_project = FactoryGirl.create(:user_project, :in_progress, project: project)
+      expect(subject).to_not include user_project
+    end
+    it "returns user projects that are complete" do
+      user_project = FactoryGirl.create(:user_project, :complete, project: project)
+      expect(subject).to include user_project
+    end
+
+    it "does not return user_projects for the user" do
+      user_project = FactoryGirl.create(:user_project, :complete, project: project)
+      user = user_project.user
+      expect(ProjectPresenter.new(project, user).completed_user_projects).to_not include user_project
+    end
+  end
 end
