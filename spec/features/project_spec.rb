4c34bcf2bd95713ab2fec5e42e8565f7406f5799
Sarah Pinder <r.s.pinder@gmail.com>
2013-07-14 10:16:36 +0100

    Put some more detail into the working-users list.
    Show avatar, when started, and percent completion.
    Refactor the spec a little & change it's wording.

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -2,34 +2,31 @@ require 'spec_helper'
 
 
 feature "Viewing a project someone has started" do
-  background do
-    OmniAuth.config.add_mock(:github, example_omniauth_github_response)
-  end
-
   let(:user) { FactoryGirl.create(:user, name: "Grace H") }
   let(:project) {FactoryGirl.create(:project, started_by: [user])}
 
   scenario "a guest user can see them" do
     visit project_path(project)
 
-    expect(page).to have_content("#{user.name} is working on this")
+    expect(page).to have_content("#{user.name} started work on this")
   end
 
   scenario "a logged in user can see them" do
+    OmniAuth.config.add_mock(:github, example_omniauth_github_response)
     log_in
 
     visit project_path(project)
 
-    expect(page).to have_content("#{user.name} is working on this")
+    expect(page).to have_content("#{user.name} started work on this")
   end
 
   scenario "a logged in user who has started this project can't see themselves" do
+    OmniAuth.config.add_mock(:github, omniauth_github_response_for(user))
     log_in
 
     visit project_path(project)
-    click_button "I want to start work on this"
 
-    expect(page).to have_no_content("#{example_omniauth_github_response.info.name} is working on this")
+    expect(page).to have_no_content("#{user.name} started work on this")
   end
 end
 
